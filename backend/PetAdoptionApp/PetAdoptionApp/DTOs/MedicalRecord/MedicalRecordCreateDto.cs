dbc74510e091bafbbd98e3ae844e8070446f9ddc
Mila Jovic <mila.jovic@elfak.rs>
2026-04-09 18:45:54 +0200

    prvi dto

---
+++ backend/PetAdoptionApp/PetAdoptionApp/DTOs/MedicalRecord/MedicalRecordCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PetAdoptionApp.DTOs.MedicalRecord
+{
+    public class MedicalRecordCreateDto
+    {
+        public string description { get; set; }
+        public DateTime date { get; set; }
+        public string clinicPhone { get; set; }
+        public string vetName { get; set; }
+        public DateTime nextDueDate { get; set; }
+        public string[]? vaccines { get; set; }
+    }
+}
