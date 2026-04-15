524ff0eb38084c5e0bcb2d2707d72fbdf140e034
Mila Jovic <mila.jovic@elfak.rs>
2026-04-15 18:30:35 +0200

    pocetak kontrolera

--- backend/PetAdoptionApp/PetAdoptionApp/Controllers/AuthController.cs
+++ backend/PetAdoptionApp/PetAdoptionApp/Controllers/AuthController.cs
@@ -1,6 +1,33 @@
-﻿namespace PetAdoptionApp.Controllers
+﻿using PetAdoptionApp.DTOs.Auth;
+using PetAdoptionApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PetAdoptionApp.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            try
+            {
+                var result = await _authService.LoginUserAsync(dto);
+                return Ok(result);
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                return Unauthorized(e.Message);
+            }
+        }
     }
 }
